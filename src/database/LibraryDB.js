aa9e450d83e5996aa8131559fe2af0c0d31fef45
dgusfr <diegogustavo.fr@gmail.com>
2024-10-19 22:14:01 -0300

    update

---
+++ src/database/LibraryDB.js
@@ -0,0 +1,17 @@
+// database/LibraryDB.js
+/*
+classe biblioteca 
+ construtor{
+  livros []
+  membros []
+  emprestimos []
+ }
+
+ adiciona_livro(liivro){
+ em livros push livro
+ retorne o livro
+ }
+
+ pega_livros()
+
+*/
