fd75730fde119ad51df5c3803d08949dea9dfaed
dgusfr <diegogustavo.fr@gmail.com>
2024-10-20 10:59:57 -0300

    feat(algoritmo): livro controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,31 @@
+const LibraryDB = require("../database/LibraryDB");
+const Book = require("../models/Book");
+/*
+classe Livrocontroler :
+ estatico adicioLivro(id, titulo, autor, ano, e copia){
+  novo livro recebe new livro(id, titulo, autor, ano, e copia)
+  livraryDB.adicionalivo(novolicro)
+  "LIvro adicionado com sucesso"
+ }
+
+ estativco pegalivros(){
+ retorna livrariaDB.égalivros()
+ }
+
+ estatico atualiza livros(id,atualizaDados){
+ tente{
+ atualizalivro recebe livrariadb.atualizalivro(id, atualizaDados)}
+ "Livro atualizado"
+ retuna atualizalivro
+ }
+
+ estatico deletalivbro(id){
+ tente{
+  livrariaDB.removelivro(id)
+  "Livro removido"
+  } catch (error) {
+   "(error.messagem)"
+   }
+  }
+ mudulo.exporta = livroController
+ */
