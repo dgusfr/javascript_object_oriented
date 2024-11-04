e0573b7f53ebedfaf9564dcb011b0071d856e251
dgusfr <diegogustavo.fr@gmail.com>
2024-11-04 11:27:50 -0300

    feat(bookController)

--- scripts/bookController.js
+++ scripts/bookController.js
@@ -1,33 +1,10 @@
-const LibraryDB = require("./LibraryDB");
-const Book = require("../src/models/Book");
-
-class BookController {
-  static addBook(id, title, author, year, copies) {
-    const newBook = new Book(id, title, author, year, copies);
-    LibraryDB.addBook(newBook);
-    console.log(`Livro ${title} adicionado com sucesso`);
-  }
-
-  static getBooks(filterFn = null) {
-    return LibraryDB.getBooks(filterFn);
-  }
-
-  static updateBook(id, updatedData) {
-    try {
-      const updatedBook = LibraryDB.updateBook(id, updatedData);
-      console.log(`Livro de ID ${id} atualizado com sucesso.`);
-      return updatedBook;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  static deleteBook(id) {
-    try {
-      LibraryDB.removeBook(id);
-      console.log(`Livro de id ${id} removido com sucesso`);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+function addBook(id, title, author, year, copies) {
+  libraryDB.books.push({
+    id,
+    title,
+    author,
+    year,
+    copies,
+    availableCopies: copies,
+  });
 }
