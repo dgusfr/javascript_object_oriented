4401430d1610a15332d04b27917ebab92b4ea29d
dgusfr <diegogustavo.fr@gmail.com>
2024-11-04 11:28:10 -0300

    styles(memberController)

--- scripts/memberController.js
+++ scripts/memberController.js
@@ -1,35 +1,3 @@
-const LibraryDB = require("../database/LibraryDB");
-const Member = require("../src/models/Member");
-
-class MemberController {
-  static addMember(id, name, email) {
-    const newMember = new Member(id, name, email);
-    LibraryDB.addMember(newMember);
-    console.log(`Membro "${name}" adicionado com sucesso.`);
-  }
-
-  static getMembers(filterFn = null) {
-    return LibraryDB.getMembers(filterFn);
-  }
-
-  static updateMember(id, updatedData) {
-    try {
-      const updatedMember = LibraryDB.updateMember(id, updatedData);
-      console.log(`Membro de ID ${id} atualizado com sucesso.`);
-      return updatedMember;
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  static deleteMember(id) {
-    try {
-      LibraryDB.removeMember(id);
-      console.log(`Membro de ID ${id} removido com sucesso.`);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
+function addMember(id, name, email) {
+  libraryDB.members.push({ id, name, email });
 }
-
-module.exports = MemberController;
