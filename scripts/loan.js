9776323054928d5d6e681832ef85a167b2cd51a3
dgusfr <diegogustavo.fr@gmail.com>
2024-11-04 11:33:14 -0300

    feat(appp)

--- scripts/loan.js
+++ scripts/loan.js
@@ -1,41 +1,13 @@
-function addLoan(id, bookId, memberId) {
-  const book = libraryDB.books.find((book) => book.id === bookId);
-  const member = libraryDB.members.find((member) => member.id === memberId);
-
-  if (!book || !member) {
-    alert("Livro ou membro não encontrado!");
-    return;
+class Loan {
+  constructor(id, bookId, memberId, loanDate = new Date(), returnDate = null) {
+    this.id = id;
+    this.bookId = bookId;
+    this.memberId = memberId;
+    this.loanDate = loanDate;
+    this.returnDate = returnDate;
   }
 
-  if (book.availableCopies <= 0) {
-    alert("Não há cópias disponíveis para empréstimo.");
-    return;
+  closeLoan() {
+    this.returnDate = new Date();
   }
-
-  book.availableCopies -= 1;
-  libraryDB.loans.push({
-    id,
-    bookId,
-    memberId,
-    loanDate: new Date(),
-    returnDate: null,
-  });
-  alert("Empréstimo registrado com sucesso.");
-}
-
-function closeLoan(id) {
-  const loan = libraryDB.loans.find((loan) => loan.id === id);
-
-  if (!loan || loan.returnDate) {
-    alert("Empréstimo não encontrado ou já encerrado.");
-    return;
-  }
-
-  const book = libraryDB.books.find((book) => book.id === loan.bookId);
-  if (book) {
-    book.availableCopies += 1;
-  }
-
-  loan.returnDate = new Date();
-  alert("Empréstimo encerrado com sucesso.");
 }
