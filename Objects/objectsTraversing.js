09e921a36ca1a8c43983c961168a81944c5b1487
dgusfr <diegogustavo.fr@gmail.com>
2024-12-21 12:05:14 -0300

    feat add conditional

--- Objects/objectsTraversing.js
+++ Objects/objectsTraversing.js
@@ -19,6 +19,8 @@ const student = {
 
 for (let key in student) {
   const type = typeof student[key];
-  const text = `The Key is: ${key} and the value is: ${student[key]}`;
-  console.log(text);
+  if(type != 'object' && type != 'function') {
+    const text = `The Key is: ${key} and the value is: ${student[key]}`;
+    console.log(text);
+  }
 }
