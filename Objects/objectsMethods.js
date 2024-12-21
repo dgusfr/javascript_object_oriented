d20326e309203b14d7271432494b3d8a5376ded6
dgusfr <diegogustavo.fr@gmail.com>
2024-12-21 12:09:48 -0300

    feat add

---
+++ Objects/objectsMethods.js
@@ -0,0 +1,20 @@
+const student = {
+  name: 'José',
+  age: 27,
+  cpf: '12345678912',
+  position: 'developer',
+  telephone: ['5511999999999', '5511888888888'],
+  address: [ {
+    street: 'R. Joseph Climper',
+    number: '45',
+    complement: 'apt. 43'
+  },
+  {
+    street: 'R. Cecilia Pipper',
+    number: '525',
+    complement: null
+  },
+  ]
+}
+
+const keysOfObject = Object.keys(student);
