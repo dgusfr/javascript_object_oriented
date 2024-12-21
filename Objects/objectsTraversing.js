d597b557b891451b3c7e0191dac46ccd0c08d180
dgusfr <diegogustavo.fr@gmail.com>
2024-12-21 11:56:16 -0300

    feat add

---
+++ Objects/objectsTraversing.js
@@ -0,0 +1,12 @@
+const student = {
+  name: 'José',
+  age: 27,
+  cpf: '12345678912',
+  position: 'developer',
+  telephone: ['5511999999999', '5511888888888'],
+  address: [ {
+    street: 'R. Joseph Climper',
+    number: '45',
+    complement: 'apt. 43'
+  }]
+}
