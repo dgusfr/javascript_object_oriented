05278fe280d95e40446e8689df74f46e09fb6723
dgusfr <diegogustavo.fr@gmail.com>
2024-12-21 11:42:19 -0300

    feat update

--- Objects/objectList.js
+++ Objects/objectList.js
@@ -3,5 +3,10 @@ const student = {
   age: 27,
   cpf: '12345678912',
   position: 'developer',
-  telephone: ['5511999999999', '5511888888888']
+  telephone: ['5511999999999', '5511888888888'],
+  address: [ {
+    street: 'R. Joseph Climper',
+    number: '45',
+    complement: 'apt. 43'
+  }]
 }
